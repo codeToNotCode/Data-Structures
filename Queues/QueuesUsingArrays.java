52ec46d6753a0cbc2b0dae163e7fc4f641ea1742
Prasan <prasan.ubhi@gmail.com>
2016-12-19 02:32:28 -0500

    Working on Stacks and Queues

---
+++ Queues/QueuesUsingArrays.java
@@ -0,0 +1,109 @@
+/*
+ * Implement Queues using arrays
+ */
+
+
+package Queues;
+
+public class QueuesUsingArrays 
+{
+
+	static Object queue[] = new Object[10];
+	static int head = -1;
+	static int tail = -1;
+	
+	//To check if the queue is empty
+	public static boolean isEmpty()
+	{
+		return ( head == -1 || tail == -1 || head>tail )  ;
+	}
+	
+	//To check if the queue is full
+	public static boolean isFull()
+	{
+		return tail == queue.length - 1 ;
+	}
+	
+	//To display the topmost element of the queue
+	public static Object peek()
+	{
+		return queue[head];
+	}
+	
+	//To enqueue elements unto a queue
+	public static void enqueue(Object data)
+	{
+		//Check for full queue
+		if( isFull() )
+		{
+			System.err.println("Error: queue is full! Cannot add more elements");
+			return;
+		}
+		else if ( isEmpty() )
+		{
+			queue[++tail] = data;
+			++head;
+		}
+		else
+			queue[++tail] = data;
+		
+	}
+	
+	//To dequeue an element out of the queue
+	public static Object dequeue()
+	{
+		//Check for Empty queue
+		if( isEmpty() )
+		{
+			System.err.println("Error: queue is empty! Cannot remove from an Empty queue");
+			return null;
+		}
+		
+		return queue[head++];
+	}
+	
+	//To display queue elements
+	public static void display()
+	{
+		//check for empty queue
+		if( isEmpty() )
+		{
+			System.err.println("Error: queue is Empty");
+			return;
+		}
+		
+		for( int i = head ; i <= tail ; i++)
+		{
+			System.out.println(queue[i]);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		enqueue("Hi");
+		enqueue("My");
+		enqueue("Name");
+		enqueue("is");
+		enqueue("Prasan");
+		enqueue("Deep");
+		enqueue("Singh");
+		enqueue("Ubhi");
+		enqueue("How");
+		enqueue("You");
+		enqueue("Doing");
+		display();
+		System.out.println("Element at the front of the queue :"+peek());
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+		dequeue();display();
+	}
+
+}
--- Stacks/StackUsingArrays.java
+++ Stacks/StackUsingArrays.java
@@ -7,21 +7,108 @@ package Stacks;
 public class StackUsingArrays 
 {
 	
-	int size;
-	int stack[] = new int[size];
-	int popIndex,pushIndex;
+	static Object stack[] = new Object[10];
+	static int top = -1;
 	
-	StackUsingArrays(int s)
+	//To check if the stack is empty
+	public static boolean isEmpty()
 	{
-		size = s;
-		popIndex = 0 ;
-		pushIndex = 0;
+		return top == -1 ;
+	}
+	
+	//To check if the stack is full
+	public static boolean isFull()
+	{
+		return top == stack.length - 1 ;
+	}
+	
+	//To display the topmost element of the stack
+	public static Object peek()
+	{
+		return stack[top];
+	}
+	
+	//To push elements unto a stack
+	public static void push(Object data)
+	{
+		//Check for full stack
+		if( isFull())
+		{
+			System.err.println("Error: Stack is full! Cannot add more elements");
+			return;
+		}
+		else
+			stack[++top] = data; 
+	}
+	
+	//To pop an element out of the stack
+	public static Object pop()
+	{
+		//Check for Empty Stack
+		if( isEmpty() )
+		{
+			System.err.println("Error: Stack is empty! Cannot pop from an Empty stack");
+			return null;
+		}
+		
+		return stack[top--];
+	}
+	
+	//To display stack elements
+	public static void display()
+	{
+		//check for empty stack
+		if( isEmpty() )
+		{
+			System.err.println("Error: Stack is Empty");
+			return;
+		}
+		
+		for( int i = top ; i != -1 ; i--)
+		{
+			System.out.println(stack[i]);
+		}
 	}
 	
 	
 	public static void main(String[] args)
 	{
 		
+		push("Hi");
+		push("My");
+		push("Name");
+		push("is");
+		push("Prasan");
+		push("Deep");
+		push("Singh");
+		push("Ubhi");
+		push("How");
+		push("You");
+		push("Doing");
+		display();
+		System.out.println("Element at the top of the stack :"+peek());
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
+		pop();
+		display();
 	}
 
 }
